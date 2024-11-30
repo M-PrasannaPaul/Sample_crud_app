e498cb14a61f498c66a005d0c35a90de2d151c1b
M-PrasannaPaul <madagala.prasanna@tudip.com>
2024-11-30 11:24:10 +0530

    Update insert_log.py

--- log_monitoring/app/insert_log.py
+++ log_monitoring/app/insert_log.py
@@ -1,7 +1,6 @@
 import mysql.connector
 import json
 
-
 db_config = {
     'user': 'mysqladmin',
     'password': 'Paul*1928',
@@ -9,29 +8,37 @@ db_config = {
     'database': 'log_monitoring'
 }
 
-connection = mysql.connector.connect(**db_config)
-cursor = connection.cursor()
 
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS nginx_logs (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        timestamp VARCHAR(255),
-        client_ip VARCHAR(255),
-        request_uri VARCHAR(255),
-        status_code INT
-    )
-''')
-connection.commit()
+def create_table_if_not_exists():
+    try:
+        with mysql.connector.connect(**db_config) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS nginx_logs (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        timestamp VARCHAR(255),
+                        client_ip VARCHAR(255),
+                        request_uri VARCHAR(255),
+                        status_code INT
+                    )
+                ''')
+                connection.commit()
+    except mysql.connector.Error as err:
+        print(f"Error creating table: {err}")
 
 
 def insert_log(log_entry):
-    query = '''
-        INSERT INTO nginx_logs (timestamp, client_ip, request_uri, status_code)
-        VALUES (%s, %s, %s, %s)
-    '''
-    cursor.execute(query, (log_entry['time'], log_entry['remote_ip'], log_entry['request'], log_entry['response']))
-    connection.commit()
+    try:
+        with mysql.connector.connect(**db_config) as connection:
+            with connection.cursor() as cursor:
+                query = '''
+                    INSERT INTO nginx_logs (timestamp, client_ip, request_uri, status_code)
+                    VALUES (%s, %s, %s, %s)
+                '''
+                cursor.execute(query, (log_entry['time'], log_entry['remote_ip'], log_entry['request'], log_entry['response']))
+                connection.commit()
+    except mysql.connector.Error as err:
+        print(f"Error inserting log: {err}")
 
 
 def parse_and_store_logs(log_file, max_logs=500):
@@ -45,9 +52,8 @@ def parse_and_store_logs(log_file, max_logs=500):
             insert_log(log_entry)
             count += 1  
 
-log_file = '/app/app/nginx_json_logs.txt'
-parse_and_store_logs(log_file)
 
-
-cursor.close()
-connection.close()
+if __name__ == "__main__":
+    create_table_if_not_exists()
+    log_file = '/app/app/nginx_json_logs.txt'
+    parse_and_store_logs(log_file)
